dfeeac8db2c8bb6ae11a63ca59a7de0e5d3eedda
jelly <1355532189@qq.com>
2025-08-12 22:59:43 +0800

    finish week-1 day-5 task-2

--- src/tiny_llm/embedding.py
+++ src/tiny_llm/embedding.py
@@ -1,12 +1,26 @@
 import mlx.core as mx
+from .basics import linear
 
 
 class Embedding:
     def __init__(self, vocab_size: int, embedding_dim: int, weight: mx.array):
-        pass
+        self.vocab_size = vocab_size
+        self.embedding_dim = embedding_dim
+        self.weight = weight    # (vocab_size x embedding_dim)
 
     def __call__(self, x: mx.array) -> mx.array:
-        pass
+        '''
+        Input: N.. (tokens)
+        Output: N.. x embedding_dim (vectors)
+        '''
+        # This can be done with a simple array index lookup operation.
+        return self.weight[x, :]
 
     def as_linear(self, x: mx.array) -> mx.array:
-        pass
+        '''
+        In the Qwen2 model, the embedding layer can also be used as a linear layer to map the embeddings back to the token space.
+        
+        Input: N.. x embedding_dim
+        Output: N.. x vocab_size
+        '''
+        return linear(x, self.weight)
