a0ffe677eb89677ecdd338ff69e2e70016be8b90
jelly <1355532189@qq.com>
2025-08-17 16:03:52 +0800

    finish week-1 day-6

--- src/tiny_llm/generate.py
+++ src/tiny_llm/generate.py
@@ -11,8 +11,30 @@ def simple_generate(
     prompt: str,
     sampler: Callable[[mx.array], mx.array] | None,
 ) -> str:
-    pass
+    # You only need the last token's logits to decide the next token. 
+    # Therefore, you need to select the last token's logits from the output logits.
+    def _step(model, y, offset):
+        out = model(y[None], offset)[:, -1, :]   # (N, L, E), here batch_size=1 by using y[None]
+        output_probs = out - mx.logsumexp(out, keepdims=True)
+        if sampler is None:
+            return mx.argmax(output_probs, axis=-1)
+        else:
+            return sampler(output_probs)
+    
+    # prompt => tokens
+    token_ids = mx.array(tokenizer._tokenizer.encode(prompt))   # shape (L,)
+    detokenizer = tokenizer._detokenizer
+    detokenizer.reset()
 
+    while True:
+        tk = _step(model, token_ids, 0) # Whatever the value of offset is
+        mx.eval(tk)
+        token_ids = mx.concat([token_ids, tk])
+        if tk.item() == tokenizer._tokenizer.eos_token_id:
+            break
+        detokenizer.add_token(tk.item())
+        print(detokenizer.last_segment, end="", flush=True)
+        
 
 def simple_generate_with_kv_cache(
     model: Qwen2ModelWeek2, tokenizer: TokenizerWrapper, prompt: str
