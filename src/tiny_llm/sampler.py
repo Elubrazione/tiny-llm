83331781568c537e92b72da51dc054b2b5722d16
jelly <1355532189@qq.com>
2025-08-17 17:31:46 +0800

    finish week-1 day-7

--- src/tiny_llm/sampler.py
+++ src/tiny_llm/sampler.py
@@ -6,6 +6,21 @@ def make_sampler(temp: float, top_p: float, top_k: int | None):
     def sample(logprobs: mx.array):
         if temp == 0:
             return mx.argmax(logprobs, axis=-1)
-        pass
+        
+        # keep the top-k tokens with the highest probabilities before sampling the probabilities
+        if top_k is not None and top_k > 0:
+            masked_idx = mx.argpartition(-logprobs, kth=top_k - 1, axis=-1)[:, top_k:]
+            logprobs[:, masked_idx] = -mx.inf
 
+        # keep the top-p tokens with the highest cumulative probabilities before sampling the probabilities
+        if top_p is not None and top_p > 0:
+            idx = mx.argsort(-logprobs, axis=-1)
+            sorted_probs = logprobs[:, idx]
+            cumulative_probs = mx.cumsum(mx.exp(sorted_probs))
+            mask = cumulative_probs < top_p
+            mask[..., 0] = True
+            logprobs[:, idx] = mx.where(mask, sorted_probs, -mx.inf)
+        
+        logprobs = logprobs / temp
+        return mx.random.categorical(logprobs, axis=-1)
     return sample
